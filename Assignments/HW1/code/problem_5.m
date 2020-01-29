63811d7d2c47da4775f54b67a6bd83146b2ebe6d
Ahmed <eng.a.hassen@gmail.com>
2020-01-29 15:17:01 -0800

    HW1 problem 5

--- Assignments/HW1/code/problem_5.m
+++ Assignments/HW1/code/problem_5.m
@@ -31,41 +31,49 @@ L(:,16)
 err = normest(small_A.A - L*transpose(L))/normest(L);
 fprintf("\n relative error = %e\n\n\n", err);
 
+clc;
+
 fprintf("\n         $$$$$$ medium_ex1 $$$$$$$\n ");
 run_reorder(med_A1.A, 30, 200);
 
-fprintf("\n         $$$$$$ medium_ex2 $$$$$$$\n ");
+fprintf("\n\n\n         $$$$$$ medium_ex2 $$$$$$$\n ");
 run_reorder(med_A2.A, 30, 200);
 
 function run_reorder(A, nnzmax_factor, tlimit)
     run_chol(A, nnzmax_factor*nnz(A), tlimit,...
         '** No reorder **');
-
+    fprintf("\n");
+    
     symamd_P = symamd(A);
     run_chol(A(symamd_P,symamd_P), nnzmax_factor*nnz(A), tlimit, ...
         '** SYMAMD **');
+    fprintf("\n");
 
     colamd_P = colamd(A);
     run_chol(A(colamd_P, colamd_P), nnzmax_factor*nnz(A), tlimit, ...
         '** COLAMD **');
+    fprintf("\n");
     
     symrcm_P = symrcm(A);
     run_chol(A(symrcm_P, symrcm_P), nnzmax_factor*nnz(A), tlimit, ...
         '** SYMRCM **');
+    fprintf("\n");
     
     colperm_P = colperm(A);
     run_chol(A(colperm_P, colperm_P), nnzmax_factor*nnz(A), tlimit, ...
         '** COLPERM **');
+    fprintf("\n");
 end 
 
 function run_chol (A, nnzmax, tlimit, msg)
     [L, flag, k] = spcholesky(A, nnzmax, tlimit);
     fprintf('%s \n nnz(L) = %d, flag = %d, k= %d\n', msg, nnz(L), flag,k);
     L_large = abs(full(L(:,1000)));
-    sort(L_large,'descend');
-    fprintf('Ten largest entries in column 1000\n');
-    
-    L_large(1:10,1)
+    [L_large, L_large_row] = sort(L_large,'descend');
+    fprintf('Ten largest entries in column 1000\n');    
+    for id =1:10
+        fprintf("\n L(%d, %d) = %e", L_large_row(id), 1000,L_large(id));
+    end     
 end
 
 
