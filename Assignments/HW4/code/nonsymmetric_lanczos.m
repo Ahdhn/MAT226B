9a86282d25ec72d6ca8514fd83f83e8a2c5ea848
Ahmed <eng.a.hassen@gmail.com>
2020-03-11 20:53:59 -0700

    hw4

--- Assignments/HW4/code/nonsymmetric_lanczos.m
+++ Assignments/HW4/code/nonsymmetric_lanczos.m
@@ -1,45 +1,38 @@
-function [Tk] = nonsymmetric_lanczos(A, r, c, kmax)   
+function [Tk] = nonsymmetric_lanczos(matVecFunc, matVecTransFunc, r, c, kmax)   
     n = length(r);
     alpha = zeros(kmax,1);
     gamma = zeros(kmax+1,1);
     beta = zeros(kmax+1,1);
+    eta = zeros(kmax,1);
     
     beta(1) = norm(r);    
     gamma(1) = norm(c);
     
     vk = r./beta(1);    
     wk = c./gamma(1);
-    
-    V = zeros(n, kmax);%
-    W = zeros(n, kmax);%
-    
+            
     for k =1:kmax        
-        V(:,k) = vk;%
-        W(:,k) = wk;%
-        
-        delta_k = transpose(wk)*vk; 
+        delta_k = transpose(wk)*vk;
         
         if abs(delta_k) == 0
             break;
         end
         
-        q = A*vk;
-        s = transpose(A)*wk;
+        %q = A*vk;
+        q = matVecFunc(vk);
+        %s = transpose(A)*wk;
+        s = matVecTransFunc(wk);
         
-        if k > 1 
-            q = q - (gamma(k)*delta_k/delta_k_1).*vk_1;            
-            s = s - (beta(k)*delta_k/delta_k_1).*wk_1;   
-            transpose(wk_1)*q
-            transpose(s)*vk_1
+        if k > 1             
+            eta(k) = gamma(k)*delta_k/delta_k_1;
+            q = q - eta(k).*vk_1;            
+            s = s - (beta(k)*delta_k/delta_k_1).*wk_1;           
         end
         
         alpha(k) = transpose(wk)*q/delta_k;  
         
         q = q - alpha(k)*vk;        
         s = s - alpha(k)*wk; 
-        
-        transpose(wk)*q
-        transpose(s)*vk
         
         beta(k+1) = norm(q);        
         gamma(k+1) = norm(s);
@@ -53,8 +46,6 @@ function [Tk] = nonsymmetric_lanczos(A, r, c, kmax)
         
         vk = (1.0/beta(k+1)).*q;
         wk = (1.0/gamma(k+1)).*s;
-    end        
-    eta = alpha(1:k).*( gamma(1:k) - gamma(2:k+1) );
-    Tk = diag(alpha(1:k),0)+diag(beta(2:k),-1)+diag(eta(2:k),1);
-    DD = transpose(W)*V;
+    end            
+    Tk = diag(alpha(1:k),0)+diag(beta(2:k),-1)+diag(eta(2:k),1);    
 end
--- Assignments/HW4/code/problem_4.m
+++ Assignments/HW4/code/problem_4.m
@@ -5,9 +5,64 @@ close all;
 clf;
 format long e;
 
+global A U D1 L L_t U_t D0;
+
+mat_vec = @(x) A*x;
+mat_trans_vec = @(x) transpose(A)*x;
 
 %a)
+fprintf('\n ########## Part a):\n');
 load('HW4_P4a.mat');
+T = nonsymmetric_lanczos(mat_vec,mat_trans_vec, r, c, 5);
+fprintf('\n T eigenvalues for K = 5');
+eig(T)
+T = nonsymmetric_lanczos(mat_vec,mat_trans_vec, r, c, 10);
+fprintf('\n T eigenvalues for K = 10');
+eig(T)
+T = nonsymmetric_lanczos(mat_vec,mat_trans_vec, r, c, 20);
+fprintf('\n T eigenvalues for K = 20');
+eig(T)
+fprintf('\n norm(eig(T20)-eig(A)) = %e\n',...
+    norm(sort(abs(eig(T))) - sort(abs(eig(A)))));
 
-T = nonsymmetric_lanczos(A, r, c, 20);
-norm(sort(abs(eig(T))) - sort(abs(eig(A))))
+%b)
+fprintf('\n\n ########## Part b):\n');
+%load('HW4_P4b.mat');
+
+
+%preconditioner setup
+D0 = diag(diag(A));
+F = -tril(A,-1);
+G = -triu(A,1);
+L = D0 - F;
+U = D0 - G;
+D1 = D0 - 2*D0;
+
+L_t = transpose(D0) - transpose(F);
+U_t = transpose(D0) - transpose(G);
+
+rr = mat_vec_prime(r);
+
+A_prime_trans = transpose(A_prime);
+
+rr = mat_transpose_vec_prime(r);
+rrr = A_prime_trans*r;
+
+function out = mat_vec_prime(z)
+    global U L D0;
+    us = U\z;
+	d = diag(-D0).*us;
+	v = z + d;
+	ls = L\v;
+	sum = ls + us;
+	out = D0*sum;            
+end 
+
+function out = mat_transpose_vec_prime(z)    
+    global U_t L_t D0 D1
+    Q1 = U_t\z;
+    Q2 = L_t\z;
+    Q3 = transpose(D1)*Q2;
+    Q4 = U_t\z;
+    out = transpose(D0)*(Q1 + (Q2 + Q4));
+end
